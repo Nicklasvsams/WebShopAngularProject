0bb308e11e7de9d99fd2d12a9fcd6f477515c56a
nicklasvsams <nicklasvsams@gmail.com>
2022-02-23 12:15:38 +0100

    Generated new services and components

--- WebShopAngularProject.Client/src/app/app.module.ts
+++ WebShopAngularProject.Client/src/app/app.module.ts
@@ -8,13 +8,19 @@ import { FrontpageComponent } from './frontpage/frontpage.component';
 import { UserComponent } from './admin/user/user.component';
 import { FormsModule } from '@angular/forms';
 import { PurchaseComponent } from './admin/purchase/purchase.component';
+import { ProductComponent } from './admin/product/product.component';
+import { CategoryComponent } from './admin/category/category.component';
+import { GameComponent } from './admin/product_types/game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontpageComponent,
     UserComponent,
-    PurchaseComponent
+    PurchaseComponent,
+    ProductComponent,
+    CategoryComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
